ce365b1143d56e0f895774e636f56f7fc328295e
xuhaoteoh <56465408+xuhaoteoh@users.noreply.github.com>
2021-06-16 17:54:38 +0800

    Pending update for Search Tab
    The overall picture of this app

--- app.R
+++ app.R
@@ -3,6 +3,7 @@
 library(shiny)
 library(shinythemes)
 library(ggplot2)
+library(tidyverse)
 
 # Define UI
 ui <- fluidPage(
@@ -13,11 +14,11 @@ ui <- fluidPage(
     
     "Caloright",
     tabPanel("Introduction",
-    div(img(src="1.png"),style="text-align: center;"),
-    br(),
-    div(img(src="2.png"),style="text-align: center;"),
-    br(),
-    div(img(src="3.png"),style="text-align: center;"),
+             div(img(src="1.png"),style="text-align: center;"),
+             br(),
+             div(img(src="2.png"),style="text-align: center;"),
+             br(),
+             div(img(src="3.png"),style="text-align: center;"),
     ),
     tabPanel("Calories & Nutrition",
              sidebarPanel(
@@ -53,7 +54,7 @@ ui <- fluidPage(
                h4('Your nutritions shall be distributed as below'),
                plotOutput("GoalNutrients") #change according to output
              ) # mainPanel
-                       
+             
     )# Navbar 1, tabPanel
     ,
     tabPanel("Food Search",
@@ -71,7 +72,7 @@ ui <- fluidPage(
                tags$h5("This application shows your the nutrients and calories of your food."),
                submitButton('Search'),
                #actionButton("search", "Search", class = "btn-primary"),
-
+               
              ),
              mainPanel(
                h5('The food that is selected is'),
@@ -84,19 +85,33 @@ ui <- fluidPage(
                           tableOutput("table"),
                  ),
                  #tabPanel("Food Description",
-                          #submitButton('Search'),
-                          #uiOutput("url"),
-                          #htmlOutput("frame"),),
-                          #actionButton("searchwiki", "Search Wiki", class = "btn-primary"),),
-                          #submitButton("searchwiki"),),
+                 #submitButton('Search'),
+                 #uiOutput("url"),
+                 #htmlOutput("frame"),),
+                 #actionButton("searchwiki", "Search Wiki", class = "btn-primary"),),
+                 #submitButton("searchwiki"),),
                  tabPanel("Nutrition Distribution Graph",
                           uiOutput("url"),
                           plotOutput("plot"))
                )
              )
-    )
-    ,#Navbar 2, tabPanel 
-    tabPanel("Food Recommendation") #Navbar 2, tabPanel 
+    )#Navbar 2, tabPanel
+    , 
+    tabPanel("Food Recommended!",
+             h4("The outcome of descriptive analysis is to find out the mostrecommended for each category : Carbohydrate,Protein & Fat."),
+             
+             h4("The food with highest carbohydrate is"),
+             tableOutput("highest_carbs"),
+             plotOutput("highest_carbs_donut"),
+             
+             h4("The food with highest protein is"),
+             tableOutput("highest_protein"),
+             plotOutput("highest_protein_donut"),
+             
+             h4("The food with highest fat is"),
+             tableOutput("highest_fat"),
+             plotOutput("highest_fat_donut")
+             ) #Navbar 2, tabPanel 
   ) # navbarPage
 )# fluidPage
 
@@ -142,8 +157,8 @@ GOAL_NUTRIENTS <- function(goal,gc) {
     data <- data.frame(
       category=c("Carbohydrates", "Protein", "Fat"),
       count=c(carbs_cal,protein_cal,fat_cal)
-      )
-      
+    )
+    
     # Compute fraction
     data$fraction <- data$count / sum(data$count)
     
@@ -256,11 +271,49 @@ GOAL_NUTRIENTS <- function(goal,gc) {
   
 }
 
+DONUT_GRAPH <- function(data_x){
+  df3 <- data.frame(
+    category=c("Carbohydrates", "Protein","Fat"),
+    count=c(data_x$carbs_per_100gram,data_x$protein_per_100gram,
+            data_x$fat_per_100gram)
+  )
+  
+  # Compute fraction
+  df3$fraction <- df3$count / sum(df3$count)
+  
+  # Compute the cumulative percentages (top of each rectangle)
+  df3$ymax <- cumsum(df3$fraction)
+  
+  # Compute percentage
+  df3$percentage <- round(df3$fraction*100,digits=2)
+  
+  # Compute the bottom of each rectangle
+  df3$ymin <- c(0, head(df3$ymax, n=-1))
+  
+  # Compute label position
+  df3$labelPosition <- (df3$ymax + df3$ymin) / 2
+  
+  # Compute a good label
+  df3$label <- paste0(
+    df3$category, " (", df3$percentage, "%)", 
+    "\n Value per 100g: \n", df3$count)
+  
+  # Make the plot
+  ggplot(df3, aes(ymax=ymax, ymin=ymin, xmax=4, xmin=3, fill=category)) +
+    geom_rect() +
+    geom_label( x=3.5, aes(y=labelPosition, label=label), size=5) +
+    scale_fill_brewer(palette="Pastel1") +
+    coord_polar(theta="y") +
+    xlim(c(2, 4)) +
+    theme_void() +
+    theme(legend.position = "none")
+}
 
 options(shiny.error = browser)
 
 # Define server function  
 server <- function(input, output) {
+  library(ggplot2)
   bmr <- reactive({BMR(input$gender,input$weight,input$height,input$age)})
   output$BMRCalculation <- renderPrint(bmr())
   
@@ -283,7 +336,7 @@ server <- function(input, output) {
     }else{
       fileName <- read.csv(inFile$datapath, header = input$header)
     }
-
+    
     head(fileName)
   })
   
@@ -332,11 +385,8 @@ server <- function(input, output) {
     rownames(df2) <- 1:nrow(df2)
     
     
-    data <- df2[c(-1,-2,-3,-6,-9),]
+    data <- df2[c(-1,-2,-3,-9),]
     
-    
-    # load library
-    library(ggplot2)
     
     # Compute percentages
     
@@ -355,9 +405,88 @@ server <- function(input, output) {
       geom_rect() +
       coord_polar(theta="y") + # Try to remove that to understand how the chart is built initially
       xlim(c(2, 4)) + # Try to remove that to see how to make a pie chart
-      labs(title = paste("Top Six Componets in " , input$FoodName))
+      labs(title = paste("Top Six Componets in ",input$FoodName))
+   })
+  my_nurients_data = read.csv("D:/Teoh/Documents/R Programming/nutrients_cleaned.csv",stringsAsFactors = FALSE)
   
-  })
+  #nutrients_per_gram
+  nutrients_per_100gram = my_nurients_data %>% mutate(
+    carbs_per_100gram = 100*Carbs/Grams,
+    protein_per_100gram = 100*Protein/Grams,
+    fat_per_100gram = 100*Fat/Grams) %>% select(
+      Food,Grams,Calories,Carbs,Protein,Fat,carbs_per_100gram,
+      protein_per_100gram,fat_per_100gram)
+  
+  max_nutrients = nutrients_per_100gram
+  max_carbs = max(max_nutrients$carbs_per_100gram)
+  
+  highest_carbs= max_nutrients %>% filter(
+    carbs_per_100gram == max_carbs
+  ) %>% select(
+    Food,Carbs,Protein,Fat,
+    carbs_per_100gram,protein_per_100gram,fat_per_100gram)
+  #output$highest_carbs <- renderPrint(highest_carbs) # food with max carbs
+  
+  output$highest_carbs <- renderTable(highest_carbs)
+  
+  highest_nut_carbs <- data.frame(
+    category=c("Carbohydrates", "Protein"),
+    count=c(highest_carbs$carbs_per_100gram,highest_carbs$protein_per_100gram)
+  )
+  
+  # Compute fraction
+  highest_nut_carbs$fraction <- highest_nut_carbs$count / sum(highest_nut_carbs$count)
+  
+  # Compute the cumulative percentages (top of each rectangle)
+  highest_nut_carbs$ymax <- cumsum(highest_nut_carbs$fraction)
+  
+  # Compute percentage
+  highest_nut_carbs$percentage <- round(highest_nut_carbs$fraction*100,digits=2)
+  
+  # Compute the bottom of each rectangle
+  highest_nut_carbs$ymin <- c(0, head(highest_nut_carbs$ymax, n=-1))
+  
+  # Compute label position
+  highest_nut_carbs$labelPosition <- (highest_nut_carbs$ymax + highest_nut_carbs$ymin) / 2
+  
+  # Compute a good label
+  highest_nut_carbs$label <- paste0(
+    highest_nut_carbs$category, " (", highest_nut_carbs$percentage, "%)", 
+    "\n Value per 100g: ", highest_nut_carbs$count)
+  
+  # Make the plot
+  highest_carbs_donutplot <- ggplot(highest_nut_carbs, aes(ymax=ymax, ymin=ymin, xmax=4, xmin=3, fill=category)) +
+    geom_rect() +
+    geom_label( x=3.5, aes(y=labelPosition, label=label), size=6) +
+    scale_fill_brewer(palette="Pastel1") +
+    coord_polar(theta="y") +
+    xlim(c(2, 4)) +
+    theme_void() +
+    theme(legend.position = "none")
+  
+  output$highest_carbs_donut <- renderPlot(highest_carbs_donutplot)
+  
+  max_protein = max(max_nutrients$protein_per_100gram)
+  highest_protein= max_nutrients %>% filter(
+    protein_per_100gram == max_protein
+  ) %>% select(
+    Food,Carbs,Protein,Fat,
+    carbs_per_100gram,protein_per_100gram,fat_per_100gram)
+  highest_protein <- head(highest_protein,1)
+  
+  output$highest_protein <- renderTable(highest_protein) # food with max protein
+  
+  output$highest_protein_donut <- renderPlot(DONUT_GRAPH(highest_protein))
+  
+  max_fat = max(max_nutrients$fat_per_100gram)
+  highest_fat= max_nutrients %>% filter(
+    fat_per_100gram == max_fat
+  ) %>% select(
+    Food,Carbs,Protein,Fat,
+    carbs_per_100gram,protein_per_100gram,fat_per_100gram)
+  highest_fat <- head(highest_fat,1)
+  output$highest_fat <- renderTable(highest_fat) # food with max fat
+  output$highest_fat_donut <- renderPlot(DONUT_GRAPH(highest_fat))
 } # server
 
 
